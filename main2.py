1f1bf9accca757654811242dc5ee0df8cfd7e570
Gerald Fuchs <gerald@fuchsclan.de>
2018-04-25 16:59:35 +0200

    tread to be cleaned

--- main2.py
+++ main2.py
@@ -3,14 +3,15 @@ from googleplaces import GooglePlaces, types, lang, GooglePlacesError
 #from pandas import *
 from collections import defaultdict
 import time 
-from multiprocessing import Queue
+import queue
 from threading import Thread, Event
 
 
 # lock to serialize console output
 #lock = threading.Lock()
-que = Queue()
-error_q = Queue
+
+que = queue.Queue()
+worker_q = queue.Queue()
 threads_list = list()
 
 
@@ -19,29 +20,34 @@ threads_list = list()
 
 YOUR_API_KEY = 'AIzaSyD3wdGHiewoTV3iAGadKK7WkCLwqdhjyJs'
 
+MAX_THREADS = 1
+
 google_places = GooglePlaces(YOUR_API_KEY)
 
 def read_Exel():
     #xl = pd.ExcelFile("\\\\sv022181\\abl$\\abl\\Austausch\\Gerald\\Vertragspartner\\Arztverzeichnis-work-gesammt00.xlsx")
-    xl = pd.ExcelFile("./Arztverzeichnis-work-gesammt.xlsx")
+    xl = pd.ExcelFile("./Arztverzeichnis-work-5000.xlsx")
     
     df = xl.parse("Arztverzeichnis 01.01.2018")
     xl.close()
-    max_rows = 2500
-    dataframes = []
-    while len(df) > max_rows:
-        top = df[:max_rows]
-        top.reset_index(inplace=True, drop=True)
-        dataframes.append(top)
-        df = df[max_rows:]
-    else:
-        df.reset_index(inplace=True, drop=True)
-        dataframes.append(df)
-    print ('running {} programms in Parallel, each Programm performs {} searches. '.format (len(dataframes), max_rows))
+   
+    print ('running {} programms in Parallel, handling  {} searches. '.format (MAX_THREADS, len(df)))
+
+    resultDataFrames = traverse(df)
+    rdf= pd.concat(resultDataFrames)    
+    #target = traverse(df)
+    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    #writer = pd.ExcelWriter('\\\\sv022181\\abl$\\abl\\Austausch\\Gerald\\Vertragspartner\\Arztverzeichnis-work-google.xlsx')
     
-    for f in dataframes:
-        print ('framezize:{}-- maxindex:{}\n'.format(len(f),f.index[len(f)-1]))
     
+    writer = pd.ExcelWriter('./Arztverzeichnis-work-5000-google.xlsx')
+    rdf.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+
+    return
+   
+   
+   
     for f in dataframes:
    
         t = Thread(target=lambda q, arg1, eq: q.put(traverse(arg1,eq)), args=(que, f, error_q))
@@ -65,25 +71,42 @@ def read_Exel():
     #writer = pd.ExcelWriter('\\\\sv022181\\abl$\\abl\\Austausch\\Gerald\\Vertragspartner\\Arztverzeichnis-work-google.xlsx')
     
     
-    writer = pd.ExcelWriter('./Arztverzeichnis-work-gesammt-google.xlsx')
+    writer = pd.ExcelWriter('./Arztverzeichnis-work-5000-google.xlsx')
     rdf.to_excel(writer, sheet_name='Sheet1')
     writer.save()
 
-def traverse(df, err_q):
-    
+def traverse(df):    
     cols = d = defaultdict(list)
     df_target = df
+    for i in range (MAX_THREADS):
+        #t = threading.Thread(target=worker)
+        t = Thread(target=lambda q, wq: q.put(worker(wq)), args=(que, worker_q))
+        t.daemon=True
+        t.start()
+        threads_list.append(t)
 
     i = 0
     #for index, row in df.iterrows():
     while i < len(df):
         row = df.iloc[i]
         index = df.index[i]
-        # wait until the que has an signal to work, if the que is empty some thread has seen an error
-        err_q.get()
-        df_target = googSearch(row, df, index, cols, df_target, err_q)
-        if df_target is not  None:
-            i = i +1
+        worker_q.put(row)
+        i = i +1
+    
+    # Wait until que is empty again. 
+    worker_q.join()
+
+    #Kill each thread
+    for i in range (MAX_THREADS):
+        worker_q.put(None)
+
+    for t in threads_list:
+        t.join()
+    resultDataFrames = []
+    while not que.empty():
+        resultdf = que.get()
+        resultDataFrames.append(resultdf)
+    return resultDataFrames
     df_target['goog_name'] = cols['goog_name']
     df_target['goog_plz'] = cols['goog_plz'] 
     df_target['goog_web'] = cols['goog_web'] 
@@ -130,6 +153,126 @@ def getPlacesOrt(place):
             return ac['long_name']
     return ''
 
+
+def worker(worker_q):
+    while True:
+        
+        row = worker_q.get()
+        if row is None:
+            break
+        print (row)  
+        if pd.isna(row['Titel']):
+            row['Titel'] = ''
+
+        if pd.isna(row['Ort']):
+            row['Ort'] = ''
+            
+        #searchstr = "{} {} {}".format(row['Titel'],row['Vorname'],row['Nachname'])
+        searchstr = row['Titel']+' '+row['Vorname']+' '+row['Nachname']+', '+row['Ort']
+        
+        s_result = do_google_search(searchstr)
+        if s_result == None:
+            return None
+    
+
+        if len(s_result.places) == 0:
+            # nothing found, try it without "Ort"
+            searchstr = row['Titel']+' '+row['Vorname']+' '+row['Nachname']+', '
+            event_is_set = e.wait()
+            s_result = do_google_search(searchstr)
+
+
+        if len(s_result.places) == 0:
+            return emptyCols(cols, target_DF)
+        i = 0
+        while i < len(s_result.places):
+            place = s_result.places[i]
+        #for idx, place in enumerate(s_result.places):
+            idx = i
+            print ('{} verarbeite Zeile {} {} '.format(time.strftime("%d.%m.%Y %H:%M:%S"),index+1,place.name))
+            #print (place.place_id)
+            #print ()
+            try:
+                event_is_set = e.wait()
+                place.get_details()
+                e.set()
+            except GooglePlacesError as error_detail:
+                if str(error_detail).find('OVER_QUERY_LIMIT') >= 0:
+                    print ('going to sleep for an hour')
+                    e.clear() 
+                    time.sleep(3600)
+                    e.set()
+                    break
+                else:
+                    print (str(error_detail))
+                    break
+        
+            
+        
+            plz = getPlacesPLZ(place.details['address_components'])
+            ort = getPlacesOrt(place.details['address_components'])
+            hausnummer = getPlacesHN(place.details['address_components'])
+            strasse = getPlacesStrasse(place.details['address_components'])
+
+            if 'international_phone_number' in place.details:
+                phone = place.details['international_phone_number']
+            else:
+                phone =''
+            
+            name = place.name
+            sizeDiff = len(target_DF.values)- len(googdf.values)
+
+            if idx > 0:
+                try:
+                    #print ('index:{}\nsizeDiff:{}\nidx:{}\n'.format(index,sizeDiff,idx))
+                    s1 = googdf.iloc[index]
+                    s2 = target_DF.iloc[0:index+sizeDiff]
+                    s3 = target_DF.iloc[index+sizeDiff:]
+                    s4 = pd.concat([s1.to_frame().T],ignore_index=True)
+                    target_DF = pd.concat([s2,s4, s3],ignore_index=True)
+                except:
+                    print ('index:{}\nsizeDiff:{}\nidx:{}\n'.format(index,sizeDiff,idx))
+
+            if 'website' in place.details:
+                web = place.details['website']
+            else:
+                web =''
+            if 'rating' in place.details:
+                rating = place.details['rating']
+            else:
+                rating = ''
+            adr_formatiert = place.details['formatted_address']
+
+            if('types' in place.details):
+                types = ','.join(place.details['types'])
+            else:
+                types = ''
+
+            if('url' in place.details):
+                url = place.details['url']
+            else:
+                url = ''
+
+            if pd.isna(row['PLZ']):
+                row['PLZ'] = 0
+            plzdiff =  abs(int(row['PLZ']) - int(plz))
+
+            cols['goog_name'].append(name)
+            cols['goog_plz'].append(plz) 
+            cols['goog_web'].append(web) 
+            cols['goog_ort'].append(ort)
+            cols['goog_phone'].append(phone)
+            cols['goog_hausnummer'].append(hausnummer)
+            cols['goog_strasse'].append(strasse)
+            cols['goog_rating'].append(rating)
+            cols['goog_adr_formatiert'].append(adr_formatiert )
+            cols['goog_types'].append(types)
+            cols['goog_url'].append(url)
+            cols['goog_plzdiff'].append(plzdiff)
+        i = i+1
+        worker_q.task_done()
+
+    
 
 def googSearch(row, googdf, index, cols, target_DF, err_q):
     
--- test.py
+++ test.py
@@ -1,29 +1,31 @@
 import threading
 import time
+from multiprocessing import Queue
 
-# This function gets called by our thread.. so it basically becomes the thread innit..                    
-def wait_for_event(e):
+def worker():
     while True:
-        print ('\tTHREAD: This is the thread speaking, we are Waiting for event to start..')
-        event_is_set = e.wait()
-        print ('\tTHREAD:  WHOOOOOO HOOOO WE GOT A SIGNAL  : %s', event_is_set)
-        e.clear()
+        item = q.get()
+        if item is None:
+            break
+        do_work(item)
+        q.task_done()
 
-# Main code.. 
-e = threading.Event()
-t = threading.Thread(name='your_mum', 
-                     target=wait_for_event,
-                     args=(e,))
-t.start()
+q = Queue()
+num_worker_threads = 5
+threads = []
+for i in range(num_worker_threads):
+    t = threading.Thread(target=worker)
+    t.start()
+    threads.append(t)
 
-while True:
-    print ('\MAIN LOOP: still in the main loop..')
-    time.sleep(4)
-    print ('MAIN LOOP: I just set the flag..')
-    e.set()
-    print ('MAIN LOOP: now Im gonna do some processing n shi-t')
-    time.sleep(4)
-    print ('MAIN LOOP:  .. some more procesing im doing   yeahhhh')
-    time.sleep(4)
-    print ('MAIN LOOP: ok ready, soon we will repeat the loop..')
-    time.sleep(2)
+for item in source():
+    q.put(item)
+
+# block until all tasks are done
+q.join()
+
+# stop workers
+for i in range(num_worker_threads):
+    q.put(None)
+for t in threads:
+    t.join()
